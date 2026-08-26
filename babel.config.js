b18e0e49924564e3f0df6373e2b93335198cf4a8
Dr Vivek Raskar <dr.vivekonomics@gmail.com>
2026-08-26 07:06:35 +0000

    Resolve babel-preset-expo from the Expo package so EAS Metro can bundle

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,11 @@
+const path = require("path");
+
+function expoBabelPreset() {
+  const expoRoot = path.dirname(require.resolve("expo/package.json"));
+  return require.resolve("babel-preset-expo", { paths: [expoRoot] });
+}
+
 module.exports = function (api) {
   api.cache(true);
-  return { presets: ["babel-preset-expo"] };
+  return { presets: [expoBabelPreset()] };
 };
